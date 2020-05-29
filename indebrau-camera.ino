e9a0d0cc3121ba69d11e5af3f23be30c5bddf6fe
Peter de Lange <lange@dbis.rwth-aachen.de>
2020-05-29 18:00:55 +0200

    changes..

--- indebrau-camera.ino
+++ indebrau-camera.ino
@@ -1,21 +1,50 @@
-#include <OV2640.h>
 #include <WiFi.h>
 #include <WebServer.h>
 #include <WiFiClient.h>
 
-OV2640 cam;
+#include "esp_camera.h"
+#include "esp_timer.h"
+#include "img_converters.h"
+#include "Arduino.h"
+#include "soc/soc.h"           // Disable brownout problems
+#include "soc/rtc_cntl_reg.h"  // Disable brownout problems
+#include "driver/rtc_io.h"
+
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
+
 WebServer server(80);
 
-const char *ssid = "Brauerei";
-const char *password = "gzBbNSQYqZYbh2Va";
+const char *ssid = "FRITZBox";
+const char *password = "89020965368268527211";
 unsigned long lastToggled;
 
 void handle_jpg(void)
 {
-  digitalWrite(2, HIGH); // turn on flash
   WiFiClient client = server.client();
-  cam.run();
-  digitalWrite(2, LOW); // turn off flash
+
+  camera_fb_t * fb = NULL; // pointer
+  fb = esp_camera_fb_get();
+  if (!fb) {
+    Serial.println("Camera capture failed");
+    return;
+  }
+  esp_camera_fb_return(fb);
+
   if (!client.connected())
   {
     Serial.println("fail...");
@@ -25,7 +54,7 @@ void handle_jpg(void)
   response += "Content-disposition: inline; filename=capture.jpg\r\n";
   response += "Content-type: image/jpeg\r\n\r\n";
   server.sendContent(response);
-  client.write((char *)cam.getfb(), cam.getSize());
+  client.write(fb->buf, fb->len);
   Serial.println("send image");
 }
 
@@ -49,34 +78,45 @@ void setup()
   {
     ;
   }
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
 
-  camera_config_t camera_config;
-  camera_config.ledc_channel = LEDC_CHANNEL_0;
-  camera_config.ledc_timer = LEDC_TIMER_0;
-  camera_config.pin_d0 = 17;
-  camera_config.pin_d1 = 35;
-  camera_config.pin_d2 = 34;
-  camera_config.pin_d3 = 5;
-  camera_config.pin_d4 = 39;
-  camera_config.pin_d5 = 18;
-  camera_config.pin_d6 = 36;
-  camera_config.pin_d7 = 19;
-  camera_config.pin_xclk = 27;
-  camera_config.pin_pclk = 21;
-  camera_config.pin_vsync = 22;
-  camera_config.pin_href = 26;
-  camera_config.pin_sscb_sda = 25;
-  camera_config.pin_sscb_scl = 23;
-  camera_config.pin_reset = 15;
-  camera_config.xclk_freq_hz = 20000000;
-  camera_config.pixel_format = CAMERA_PF_JPEG;
-  camera_config.frame_size = CAMERA_FS_SVGA;
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
 
-  cam.init(camera_config);
-
-  // configure flashlight led
-  pinMode(2, OUTPUT);
-  digitalWrite(2, LOW);
+  if (psramFound()) {
+    config.frame_size = FRAMESIZE_UXGA;
+    config.jpeg_quality = 10;
+    config.fb_count = 2;
+  } else {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+  }
+  // Camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    ESP.restart();
+  }
 
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
