ebdd811b37ef05b7e7dd6939f9b38ef6296df379
Peter de Lange <lange@dbis.rwth-aachen.de>
2020-05-30 19:32:28 +0200

    wifi manager
    and works with both cam versions now

--- indebrau-camera.ino
+++ indebrau-camera.ino
@@ -1,15 +1,34 @@
-#include <WiFi.h>
 #include <WebServer.h>
 #include <WiFiClient.h>
+#include <WiFiManager.h>      // for configuring in access point mode
+#include <DNSServer.h>        // needed for WiFiManager
 
 #include "esp_camera.h"
 #include "esp_timer.h"
-#include "img_converters.h"
-#include "Arduino.h"
 #include "soc/soc.h"           // Disable brownout problems
 #include "soc/rtc_cntl_reg.h"  // Disable brownout problems
-#include "driver/rtc_io.h"
 
+// two boards: TTGO T-Journal or ESP32-CAM AI-Thinker are supported
+#define ttgoBoard false
+
+#if ttgoBoard
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM     27
+#define SIOD_GPIO_NUM     25
+#define SIOC_GPIO_NUM     23
+#define Y9_GPIO_NUM       19
+#define Y8_GPIO_NUM       36
+#define Y7_GPIO_NUM       18
+#define Y6_GPIO_NUM       39
+#define Y5_GPIO_NUM       5
+#define Y4_GPIO_NUM       34
+#define Y3_GPIO_NUM       35
+#define Y2_GPIO_NUM       17
+#define VSYNC_GPIO_NUM    22
+#define HREF_GPIO_NUM     26
+#define PCLK_GPIO_NUM     21
+#else
 #define PWDN_GPIO_NUM     32
 #define RESET_GPIO_NUM    -1
 #define XCLK_GPIO_NUM      0
@@ -26,12 +45,35 @@
 #define VSYNC_GPIO_NUM    25
 #define HREF_GPIO_NUM     23
 #define PCLK_GPIO_NUM     22
+#endif
 
 WebServer server(80);
 
-const char *ssid = "FRITZBox";
-const char *password = "89020965368268527211";
 unsigned long lastToggled;
+WiFiManager wifiManager;
+const char AP_PASSPHRASE[16] = "indebrau";
+char deviceName[30];
+
+void handleNotFound()
+{
+  String message = "Server is running!\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  server.send(200, "text/plain", message);
+}
+
+void handle_config(void){
+  String message = "Portal started!\nPlease connect to Access Point\n";
+  message += deviceName;
+  message += "\n";
+  server.send(200, "text/plain", message);
+  wifiManager.startConfigPortal(deviceName, AP_PASSPHRASE);
+}
 
 void handle_jpg(void)
 {
@@ -55,29 +97,28 @@ void handle_jpg(void)
   response += "Content-type: image/jpeg\r\n\r\n";
   server.sendContent(response);
   client.write(fb->buf, fb->len);
-  Serial.println("send image");
 }
 
-void handleNotFound()
-{
-  String message = "Server is running!\n\n";
-  message += "URI: ";
-  message += server.uri();
-  message += "\nMethod: ";
-  message += (server.method() == HTTP_GET) ? "GET" : "POST";
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
-  server.send(200, "text/plain", message);
-}
 
 void setup()
 {
   Serial.begin(115200);
-  while (!Serial)
-  {
-    ;
+  // https://forum.arduino.cc/index.php?topic=613549.0
+  uint32_t chipId = 0;
+  for(int i=0; i<17; i=i+8) {
+    chipId |= ((ESP.getEfuseMac() >> (40 - i)) & 0xff) << i;
   }
+  snprintf(deviceName, 30, "IndebrauCam-%08X", chipId); // set device name
+  
+  // WiFi Manager
+  wifiManager.setTimeout(120);
+
+  if (!wifiManager.autoConnect(deviceName, AP_PASSPHRASE))
+  {
+    Serial.println("Connection not possible, timeout, restart!");
+    ESP.restart();
+  }
+
   WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
 
   camera_config_t config;
@@ -102,49 +143,47 @@ void setup()
   config.xclk_freq_hz = 20000000;
   config.pixel_format = PIXFORMAT_JPEG;
 
-  if (psramFound()) {
-    config.frame_size = FRAMESIZE_UXGA;
-    config.jpeg_quality = 10;
-    config.fb_count = 2;
-  } else {
+  #if ttgoBoard
     config.frame_size = FRAMESIZE_SVGA;
     config.jpeg_quality = 12;
     config.fb_count = 1;
-  }
+  #else
+    config.frame_size = FRAMESIZE_UXGA;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+  #endif
+
   // Camera init
   esp_err_t err = esp_camera_init(&config);
   if (err != ESP_OK) {
     Serial.printf("Camera init failed with error 0x%x", err);
     ESP.restart();
   }
-
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  lastToggled = millis();
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(500);
-    if (millis() > lastToggled + 5000)
-    {
-      Serial.println("cannot connect, rebooting");
-      ESP.restart();
-    }
-    Serial.print(F("."));
-  }
+  
   Serial.println(F("WiFi connected"));
   Serial.println("");
   Serial.println(WiFi.localIP());
 
   server.on("/", HTTP_GET, handle_jpg);
+  server.on("/config", HTTP_GET, handle_config);
+
   server.onNotFound(handleNotFound);
   server.begin();
 }
 
 void loop()
 {
-  if (WiFi.status() != WL_CONNECTED)
-  {
-    ESP.restart(); // just restart, because..why not
-  }
+      if (WiFi.status() != WL_CONNECTED)
+    {
+      /* If the wifimanager runs into a timeout, restart device
+       * Prevents being stuck in Access Point mode when Wifi signal was
+       * temporarily lost.
+       */
+      if (!wifiManager.autoConnect(deviceName, AP_PASSPHRASE))
+      {
+        Serial.println("Connection not possible, timeout, restart!");
+        ESP.restart();
+      }
+    }
   server.handleClient();
 }
